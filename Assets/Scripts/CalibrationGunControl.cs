cfe090523637c374f88212c8241b3427ee34f953
addtheletters <bzhang.2003@gmail.com>
2015-04-27 13:30:18 -0400

    calibration gun startup

---
+++ Assets/Scripts/CalibrationGunControl.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CalibrationGunControl : GunControl {
+	// Defaults to firing along the +x axis
+	// Fires incrementally at angles
+
+	private float startAltitude;
+	private float maxAltitude;
+	private float altitudeIncrement;
+
+	private float currentAltitude;
+
+	private bool roundsComplete = false;
+
+	public void FireCalibShot(){
+		Shoot();
+		currentAltitude += altitudeIncrement;
+
+		if(currentAltitude > maxAltitude){
+			ResetCalib();
+		}
+	}
+
+	public void ResetCalib(){
+		roundsComplete = true;
+		currentAltitude = startAltitude;
+	}
+
+	public static Vector3 AimVector( float angle ){
+		return new Vector3(); // TODO this
+	}
+	
+	#region implemented abstract members of GunControl
+	public override void Shoot (){
+		gun.Fire( AimVector(currentAltitude) );
+	}
+
+	public override bool ShouldFire (){
+		return false; // should never fire automatically
+	}
+	#endregion
+}
